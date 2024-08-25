089c3ecf25a441c8d58222f96873d95c8cc4d27d
samsung <demonov03@gmail.com>
2024-08-25 15:13:44 +0300

    req

---
+++ LifeSpot/Static/JS/test.js
@@ -0,0 +1,7 @@
+ let userName = prompt("Пожалуйста, введите ваше имя");
+ alert(`Приветствуем, ${userName}. В вашем имени ${userName.length} символов`)
+// Получим коллекцию всех элементов страницы
+let elements = document.getElementsByTagName('*');
+
+// Выведем результат в уведомление
+alert(`Количество элементов на странице:  ${elements.length}`);
